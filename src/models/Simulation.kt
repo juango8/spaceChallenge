ba9bfc857e6010f1dfde3cea538156d59eab8fac
Juan Flores <jgonzalofh2898@gmail.com>
2021-04-09 19:26:15 -0500

    changing "for" by "forEach" to make the code more readable

--- src/models/Simulation.kt
+++ src/models/Simulation.kt
@@ -59,10 +59,10 @@ class Simulation {
     // runSimulation then returns the total budget required to send all rockets (including the crashed ones).
     fun runSimulation(rocketList: ArrayList<Rocket>): Int {
         var totalBudget = 0
-        for (rocket in rocketList) {
+        rocketList.forEach {
             do {
-                totalBudget += rocket.cost
-            } while (rocket.launch() && rocket.land())
+                totalBudget += it.cost
+            } while (it.launch() && it.land())
         }
 
         return totalBudget
