c2e4e7951584d99442eca69f778bf2f6103203cd
Juan Flores <jgonzalofh2898@gmail.com>
2021-04-09 19:24:01 -0500

    update inline functions to make easier to read

--- src/models/Rocket.kt
+++ src/models/Rocket.kt
@@ -11,19 +11,12 @@ open class Rocket(
 
     private val itemList: MutableList<Item> = mutableListOf()
 
+    override fun launch() = true
 
-    override fun launch(): Boolean {
-        return true
-    }
-
-    override fun land(): Boolean {
-        return true
-    }
+    override fun land() = true
 
     // returns true if the rocket can carry such item or false if it will exceed the weight limit
-    final override fun canCarry(newItem: Item): Boolean {
-        return (actualCargoWeight + newItem.weight) <= capacity
-    }
+    final override fun canCarry(newItem: Item) = (actualCargoWeight + newItem.weight) <= capacity
 
     // updates the current weight of the rocket.
     final override fun carry(newItem: Item) {
